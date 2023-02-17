53a9454163f28bbf02e813fa3ecf1e66f718be3a
SonJava10Rikkei <sonjapan7@gmail.com>
2023-02-17 15:11:32 +0700

    63 - Dựng khung layout mặc định

--- gutrend_version2/src/components/Layout/HeaderOnly/index.js
+++ gutrend_version2/src/components/Layout/HeaderOnly/index.js
@@ -1,6 +1,6 @@
 import Header from "~/components/Layout/components/Header";
 
-function DefaultLayout({ children }) {
+function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
@@ -12,4 +12,4 @@ function DefaultLayout({ children }) {
     );
 }
 
-export default DefaultLayout;
+export default HeaderOnly;
--- gutrend_version2/src/components/Layout/components/Header/index.js
+++ gutrend_version2/src/components/Layout/components/Header/index.js
@@ -1,6 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles)
 function Header() {
     return (
-        <h2>Header</h2>
+        <header className={cx('wrapper')} >
+            <div className={cx('inner')}></div>
+
+        </header>
     );
 }
 
