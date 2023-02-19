9339b8dbb1905708f17ae8a897f30ebfdc8b97bc
SonJava10Rikkei <sonjapan7@gmail.com>
2023-02-19 23:32:02 +0700

    98. Xây dựng UI phần Header 2

---
+++ gutrend_version2/src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/db3a2f35dc52de6816ee785c92f4a0be~c5_300x300.webp?x-expires=1676988000&x-signature=yt9j0DIU9chCjOdwyFN5qpewJjE%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+export default AccountItem;
